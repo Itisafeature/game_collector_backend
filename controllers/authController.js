1e43624f7613762ada3fa3359ebf0aca00440809
TheDreamingTree145 <dandersonsoftware@outlook.com>
2021-09-18 11:42:55 -0400

    changed export

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,10 @@
 const { User } = require('../models');
 const errorWrapper = require('../utils/controllerErrorHandler');
 
-const createUser = errorWrapper(async (req, res, next) => {
+exports.createUser = errorWrapper(async (req, res, next) => {
   const user = await User.create(req.body.user);
   res.status(201).json({
     status: 'success',
     data: user,
-  }); 
+  });
 });
-
-exports.createUser = createUser
