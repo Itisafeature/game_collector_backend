54b54791bbb101d7668b823e561bdc6345c87137
Itisafeature <thebigdebugger@gmail.com>
2021-10-12 19:52:58 -0400

    remove log

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -4,7 +4,7 @@ const { restart } = require('nodemon');
 
 exports.getGames = errorWrapper(async (req, res, next) => {
   const games = await Game.findAll({ include: ['rating'] });
-  // console.log(games);
+
   res.status(200).json({
     status: 'success',
     data: games,
